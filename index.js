7674e04938becf4aee35302df96aa1b0a645aa2b
Levis <Levis.mwangi@student.moringaschool.com>
2025-03-08 11:41:27 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+num1= parseInt(num1);
+num2= parseInt(num2);
+multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let number;
+number = parseInt(number);
+number = Math.floor(Math.random()*10 )+1;
+let random = number;
+console.log(random);
+
+
+let mod;
+let num3 = 16;
+let num4 = 6;
+num3 = parseInt(num3);
+num4 = parseInt(num4);
+mod = num3 % num4;
+console.log(mod);
+// Output: 4
+
+let max;
+max = Math.max(1, 2, 3, 20);
+console.log(max);
